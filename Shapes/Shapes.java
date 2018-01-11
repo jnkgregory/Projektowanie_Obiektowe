07a024c416bc46e7c86bd99adc644b3bc58b3e4e
gjanakiewicz <gjanakiewicz@localhost.localdomain>
2018-01-11 19:00:01 +0100

    Wyglada dobrze, powinno byc latwiej, niz myslalem.

--- Shapes/Shapes.java
+++ Shapes/Shapes.java
@@ -3,22 +3,28 @@ import org.opencv.core.*;
 import org.opencv.imgcodecs.*;
 import java.util.*;
 
+
 // https://docs.opencv.org/java/2.4.9/org/opencv/imgproc/Imgproc.html#findContours(org.opencv.core.Mat, java.util.List, org.opencv.core.Mat, int, int)
 // https://docs.opencv.org/java/2.4.8/org/opencv/core/MatOfPoint.html
 // https://docs.opencv.org/java/2.4.8/org/opencv/core/Point.html
 // https://www.programcreek.com/java-api-examples/index.php?api=org.opencv.core.MatOfPoint
+// http://opencvexamples.blogspot.com/2013/09/find-contour.html
+// https://stackoverflow.com/questions/18581633/fill-in-and-detect-contour-rectangles-in-java-opencv
 
 public class Shapes
 {
     public static void main(String[] args)
     {
         System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+    
+        String IMG_NAME = "i.png";
+        String DEST_IMG = "src_output.png";
 
-        Mat img = Imgcodecs.imread("eclipse.png", Imgcodecs.CV_LOAD_IMAGE_GRAYSCALE);
+        Mat img = Imgcodecs.imread(IMG_NAME, Imgcodecs.CV_LOAD_IMAGE_GRAYSCALE);
         Mat hierarchy = new Mat();
         List<MatOfPoint> contours = new ArrayList<MatOfPoint>();      
         
-        Imgproc.findContours(img, contours, hierarchy, Imgproc.RETR_LIST, Imgproc.CHAIN_APPROX_NONE);
+        Imgproc.findContours(img, contours, hierarchy, Imgproc.RETR_TREE, Imgproc.CHAIN_APPROX_NONE);
 
         //System.out.println("\n[ Shapes ] INFO: contours: " + contours.get(0).rows()+ "\n");
         System.out.println("[ Shapes ] INFO: contours size: " + contours.size());
@@ -38,20 +44,30 @@ public class Shapes
             }
         }
 
+    System.out.println("Hierarchy: " +hierarchy.size());
+    for(int i = 0; i < hierarchy.rows(); ++i)
+    {
+        for(int j = 0; j < hierarchy.cols(); ++j)
+        {
+            System.out.print(hierarchy.get(i,j)[0]);
+        }
+        System.out.println(" ");
+    }
+
 
     // Test of detecting edges through Canny method
+    System.out.println("");
 
-      Mat src = Imgcodecs.imread("inside.png");
-      Mat gray = new Mat();
+    Mat src = Imgcodecs.imread(IMG_NAME);
+    Mat gray = new Mat();
 
-      Imgproc.cvtColor(src, gray, Imgproc.COLOR_BGR2GRAY);
-      Mat edges = new Mat();
+    Imgproc.cvtColor(src, gray, Imgproc.COLOR_BGR2GRAY);
+    Mat edges = new Mat();
 
-      Imgproc.Canny(gray, edges, 60, 60*3);
+    Imgproc.Canny(gray, edges, 60, 60*3);
 
-      Imgcodecs.imwrite("src_output.png", edges);
-      System.out.println(edges + " | s: " + edges.size() + " | g: " + edges.get(1, 0)[0] + " | r: " + edges.rows() + " | c: " + edges.cols());
-
+    Imgcodecs.imwrite("src_output.png", edges);
+    System.out.println(edges + " | s: " + edges.size() + " | g: " + edges.get(1, 0)[0] + " | r: " + edges.rows() + " | c: " + edges.cols());
 
     } // end of main
 }
